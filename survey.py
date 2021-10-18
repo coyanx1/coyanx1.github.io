cc15250e064525b7e55d5d6c919118b79bcfe98d
coyanx1 <91291093+coyanx1@users.noreply.github.com>
2021-10-18 10:47:15 -1000

    Update survey.py

--- survey.py
+++ survey.py
@@ -1,1 +1,6 @@
 print("This is climate survey")
+print("What plants are right for you?")
+x = input("Is the climate you live in Dry or Wet?")
+y = input("Do you want to be able to eat the plant?") #need way to log variables
+if y = yes: 
+    input("Do you want the plant to be aesthetically pleasing?")
