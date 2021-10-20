8d47fc0eec656b55d8d8166781ba59181eb4bb74
coyanx1 <91291093+coyanx1@users.noreply.github.com>
2021-10-20 09:49:06 -1000

    Update survey.py

--- survey.py
+++ survey.py
@@ -1,6 +1,63 @@
-print("This is climate survey")
 print("What plants are right for you?")
-x = input("Is the climate you live in Dry or Wet?")
-y = input("Do you want to be able to eat the plant?") #need way to log variables
-if y = yes: 
-    input("Do you want the plant to be aesthetically pleasing?")
+x = input("Do you live in a wet or dry climate?")
+if x == "dry": #dry 
+  y = input("Do you want to be able to eat the plant?") 
+  if y == "yes": #edible
+    e = input("Do you want the plant to have medicinal purposes?")
+    if e == "yes":
+      q = input("Do you want the plant to be aesthetically pleasing?")
+      if q == "yes":
+        print("You should grow ____, ______, or ______")
+      elif q == "no":
+        print("You should grow ____, ______, or ______")
+    elif e == "no":
+      q = input("Do you want the plant to be aesthetically pleasing?")
+      if q == "yes":
+        print("You should grow ____, ______, or ______")
+      elif q == "no":
+        print("You should grow ____, ______, r ______") #test
+  elif y == "no": #inedible
+    e = input("Do you want the plant to have medicinal purposes?")
+    if e == "yes":
+      q = input("Do you want the plant to be aesthetically pleasing?")
+      if q == "yes":
+        print("You should grow ____, ______, or ______")
+      elif q == "no":
+        print("You should grow ____, ______, or ______")
+    elif e == "no":
+      q = input("Do you want the plant to be aesthetically pleasing?")
+      if q == "yes":
+        print("You should grow ____, ______, or ______")
+      elif q == "no":
+        print("You should grow ____, ______, or ______")
+    
+if x == "wet": #wet
+  y = input("Do you want to be able to eat the plant?") 
+  if y == "yes": #edible
+    e = input("Do you want the plant to have medicinal purposes?")
+    if e == "yes":
+      q = input("Do you want the plant to be aesthetically pleasing?")
+      if q == "yes":
+        print("You should grow ____, ______, or ______")
+      elif q == "no":
+        print("You should grow ____, ______, or ______")
+    elif e == "no":
+      q = input("Do you want the plant to be aesthetically pleasing?")
+      if q == "yes":
+        print("You should grow ____, ______, or ______")
+      elif q == "no":
+        print("You should grow ____, ______, or ______")
+  elif y == "no": #inedible
+    e = input("Do you want the plant to have medicinal purposes?")
+    if e == "yes":
+      q = input("Do you want the plant to be aesthetically pleasing?")
+      if q == "yes":
+        print("You should grow ____, ______, or ______")
+      elif q == "no":
+        print("You should grow ____, ______, or ______")
+    elif e == "no":
+      q = input("Do you want the plant to be aesthetically pleasing?")
+      if q == "yes":
+        print("You should grow ____, ______, or ______")
+      elif q == "no":
+        print("You should grow ____, ______, or ______")
